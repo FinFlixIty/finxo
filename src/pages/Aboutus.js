7c8061fc27b098d6c9933d92933fd9e8bf9cc74d
aviraj5121 <avirajnimja12@gmail.com>
2023-10-10 03:12:14 +0530

    commit

---
+++ src/pages/Aboutus.js
@@ -0,0 +1,20 @@
+
+import Icons from '../Component/Icons';
+import Welcome from '../Component/Welcome';
+import Card from '../Component/Footercard';
+import Feedback from '../Component/Feedback';
+
+function Aboutus() {
+    return (
+        <div>
+            <div >
+                <Welcome />
+                <Icons />
+                <Card />
+                <Feedback />
+            </div>
+        </div>
+    );
+}
+
+export default Aboutus;
